dbd1cc04ea1781b6f8defffd13eb7f320ce3da96
John Chamberlin <johnchamberlin00@gmail.com>
2021-01-20 08:47:56 -0600

    mvp done

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {createStore} from 'redux'
+import {Provider} from 'react-redux'
+
+import carSaleReducer from './reducers/carSaleReducer'
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+const store = createStore(
+    carSaleReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+
+rootElement);
